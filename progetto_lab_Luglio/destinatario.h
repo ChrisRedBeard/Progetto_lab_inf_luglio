b740756b2626e519cf2118db8196ad0b39e207d5
ChrisRedBeard <134591143+ChrisRedBeard@users.noreply.github.com>
2025-07-05 17:26:30 +0200

    aggiornamento funzioni e campi in destinatario.h

--- progetto_lab_Luglio/destinatario.h
+++ progetto_lab_Luglio/destinatario.h
@@ -5,16 +5,19 @@ typedef struct
 {
     char nome[30];
     char cognome[30];
-    char telefono[15]; // formato: +39 123 456 7890
+    char telefono[17]; // formato: +39 123 456 7890, ho modificato la lunghezza a 17 per il formato  
     char via[100];     // Indirizzo (via, numero civico, ecc.)
     char citta[50];    // Città
-    char provincia[2]; // Provincia
-    char cap[5];       // CAP
+    char provincia[3]; // Provincia
+    char cap[6];       // CAP
     char email[50];    // e-mail
 
 } Destinatario; 
 
-void modifica_indirizzo(Destinatario d);
+void stampa_destinatario(Destinatario d);
+
+
+void modifica_indirizzo(Destinatario *d);
 
 //controllare prima se il pacco sia stato spedito o meno
 void modifica_destinatario(Destinatario dest);
