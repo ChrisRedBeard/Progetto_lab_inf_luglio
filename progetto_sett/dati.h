0ba10a62a3a7bd5e9d0609b74260313ae4f1fd21
ChrisRedBeard <chrisredbeard21@gmail.com>
2025-09-15 16:13:00 +0200

    prova1 correzione

--- progetto_sett/dati.h
+++ progetto_sett/dati.h
@@ -434,65 +434,12 @@ void stampa_spedizione(Spedizione s);
 int confronta_id(const void *id1, const void *id2);
 
 /**
- * @brief Convalida le spedizioni presenti nella coda.
- * Le spedizioni vengono mostrate all’utente che può decidere se convalidarle, eliminarle o lasciarle nella coda.
- * @param c Puntatore alla coda.
- */
-void convalida_spedizioni(CodaSpedizione *c);
-
-/**
  * @brief Inserisce una nuova spedizione (con eventuale richiesta all'utente).
  * @param c Puntatore alla coda.
  */
 void inserimento_spedizione(Spedizione *s);
 
 
-/**
- * @brief Rimuove i nodi duplicati dalla coda in base al campo numID.
- *
- * Questa funzione scorre la coda di spedizioni e elimina tutti i nodi
- * che hanno un campo `sped.n` (numID) già incontrato in precedenza.
- *
- * @param[in,out] coda Puntatore alla coda di spedizioni da elaborare.
- * @return void
-*/
-bool rimuovi_doppioni_coda(CodaSpedizione *coda);
-
-
-/**
- * @brief Funzione di confronto tra due spedizioni.
- *
- * Utilizzata per ordinare le spedizioni in base a criteri multipli:
- * - priorità (true prima di false)
- * - data di consegna (più vicina prima)
- * - peso (più leggero prima)
- *
- * @param a Puntatore al primo elemento (Spedizione).
- * @param b Puntatore al secondo elemento (Spedizione).
- * @return Valore negativo se a < b, positivo se a > b, 0 se uguali.
- */
-int confronta_spedizioni(const void *a, const void *b);
-
-/**
- * @brief Inserisce una spedizione in una coda mantenendo l'ordine.
- *
- * La spedizione viene inserita nella posizione corretta all'interno della coda
- * in base alla funzione di confronto `confronta_spedizioni`.
- *
- * @param ordinata Puntatore alla coda ordinata.
- * @param nuova La spedizione da inserire.
- */
-void inserisciOrdinato(CodaSpedizione *ordinata, Spedizione nuova);
-
-/**
- * @brief Ordina una coda di spedizioni secondo la priorità e la data di invio.
- *
- * Estrae tutte le spedizioni dalla coda originale, le ordina e le reinserisce
- * in ordine nella stessa coda.
- *
- * @param originale Puntatore alla coda da ordinare.
- */
-void ordinaCodaSpedizioni(CodaSpedizione *originale);
 
 /**
  * @brief Inizializza una struttura Persona impostando i campi a valori predefiniti.
--- progetto_sett/funzioni.h
+++ progetto_sett/funzioni.h
@@ -9,6 +9,8 @@
 #define RESET "\033[0m"
 #define WHITE "\033[97m"
 
+#include "dati.h"
+
 /**
  * @typedef int_pos
  * @brief Tipo definito per rappresentare posizioni o dimensioni intere positive.
@@ -62,5 +64,56 @@ void input_id(const char *prompt, char *dest, int_pos lunghezza);
  * @brief Stampa un messaggio di uscita con animazione.
  */
 void stampa_uscita();
+
+
+
+
+
+/**
+ * @brief Rimuove i nodi duplicati dalla coda in base al campo numID.
+ *
+ * Questa funzione scorre la coda di spedizioni e elimina tutti i nodi
+ * che hanno un campo `sped.n` (numID) già incontrato in precedenza.
+ *
+ * @param[in,out] coda Puntatore alla coda di spedizioni da elaborare.
+ * @return void
+*/
+bool rimuovi_doppioni_coda(CodaSpedizione *coda);
+
+
+/**
+ * @brief Funzione di confronto tra due spedizioni.
+ *
+ * Utilizzata per ordinare le spedizioni in base a criteri multipli:
+ * - priorità (true prima di false)
+ * - data di consegna (più vicina prima)
+ * - peso (più leggero prima)
+ *
+ * @param a Puntatore al primo elemento (Spedizione).
+ * @param b Puntatore al secondo elemento (Spedizione).
+ * @return Valore negativo se a < b, positivo se a > b, 0 se uguali.
+ */
+int confronta_spedizioni(const void *a, const void *b);
+
+/**
+ * @brief Inserisce una spedizione in una coda mantenendo l'ordine.
+ *
+ * La spedizione viene inserita nella posizione corretta all'interno della coda
+ * in base alla funzione di confronto `confronta_spedizioni`.
+ *
+ * @param ordinata Puntatore alla coda ordinata.
+ * @param nuova La spedizione da inserire.
+ */
+void inserisciOrdinato(CodaSpedizione *ordinata, Spedizione nuova);
+
+/**
+ * @brief Ordina una coda di spedizioni secondo la priorità e la data di invio.
+ *
+ * Estrae tutte le spedizioni dalla coda originale, le ordina e le reinserisce
+ * in ordine nella stessa coda.
+ *
+ * @param originale Puntatore alla coda da ordinare.
+ */
+void ordinaCodaSpedizioni(CodaSpedizione *originale);
 
 #endif
